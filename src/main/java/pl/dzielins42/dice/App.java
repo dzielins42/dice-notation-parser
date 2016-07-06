2439574231d747fcc6d480e3f683dd91b770a13d
Dominik Zieliński <dzielins42@gmail.com>
2016-07-06 19:36:28 +0200

    Maven project setup

---
+++ src/main/java/pl/dzielins42/dice/App.java
@@ -0,0 +1,13 @@
+package pl.dzielins42.dice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
